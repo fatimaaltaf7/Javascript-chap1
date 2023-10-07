c1f7af55d55e4ee13fd1d40811165b7187abaa55
fatimaaltaf7 <140261694+fatimaaltaf7@users.noreply.github.com>
2023-10-07 10:33:17 +0500

    Add files via upload

---
+++ chap.js
@@ -0,0 +1,20 @@
+// javascript chap 1 practice exersize 1.1
+
+
+console.log("May the Force be with you-	Star Wars")
+
+//exercie 1.2
+console.log("hello world!")
+
+// exersize 1.3
+
+alert("Assalam o Alaikum!");
+
+// exercize 1.4
+ let a = 10;
+console.log(a)
+
+
+/* this is multiple line comment */
+
+
